f17bbb2a49fedf73cf3a6876cbf09ba81e61fc53
Rafael David Quirino <rafael.david.quirino@gmail.com>
2018-07-26 10:45:13 -0300

    Started to implement the autoencoder code.

--- include/autoencoder.h
+++ include/autoencoder.h
@@ -4,15 +4,27 @@ extern "C" {
 #ifndef _AUTOENCODER_H_
 #define _AUTOENCODER_H_
 
+#include "../include/data.h"
+#include "../include/linalg.h"
 #include "../include/neuralnet.h"
 
-typedef struct {
+// typedef struct {
+//     neuralnet_t* nn;
+// } autoencoder_t;
 
-    neuralnet_t* nn;
+typedef neuralnet_t autoencoder_t;
 
-} autoencoder_t;
+autoencoder_t* ae_new  (int input_size);
+void           ae_free (autoencoder_t** ae);
 
-autoencoder_t* ae_new ();
+void ae_add_layer    (autoencoder_t* ae, int lsize);
+void ae_remove_layer (autoencoder_t* ae, int lsize);
+
+vec_t* ae_encode (autoencoder_t* ae, vec_t* vecs);
+vec_t* ae_decode (autoencoder_t* ae, vec_t* vecs);
+
+void   ae_train (autoencoder_t* ae, dataset_t* dataset);
+vec_t* ae_test  (autoencoder_t* ae, dataset_t* dataset);
 
 #endif // _AUTOENCODER_H_
 #ifdef __cplusplus
--- src/autoencoder.c
+++ src/autoencoder.c
@@ -1,1 +1,36 @@
 #include "../include/autoencoder.h"
+
+autoencoder_t* ae_new (int input_size)
+{
+    autoencoder_t* ae;
+
+    int topology[3] = {input_size,input_size/2,input_size};
+    ae = nn_new(topology,3);
+    nn_set_layer_activation(ae, 0, NN_SIGMOID_ACTIVATION);
+    nn_set_layer_activation(ae, 1, NN_SIGMOID_ACTIVATION);
+    nn_set_output_activation(ae, NN_SIGMOID_ACTIVATION);
+    nn_set_cost_function(ae, NN_SQUARE_ERROR);
+
+    return ae;
+}
+
+
+
+void ae_free (autoencoder_t** ae)
+{
+    nn_free(ae);
+}
+
+
+
+void ae_train (autoencoder_t* ae, dataset_t* dataset)
+{
+
+}
+
+
+
+vec_t* ae_test (autoencoder_t* ae, dataset_t* dataset)
+{
+    return NULL;
+}
